a3152d264c63e42e929949604f7e9e04d23f964d
Stas Ukolov <ukoloff@gmail.com>
2017-03-25 00:45:28 +0500

    Fetch full DERs

--- edge/enum.cs
+++ edge/enum.cs
@@ -9,13 +9,10 @@ public class Startup
     var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
     store.Open(OpenFlags.OpenExistingOnly);
 
-    System.Console.WriteLine("Version: " + System.Environment.Version.ToString());
-    System.Console.Beep();
-    var all = new List<string>();
+    var all = new List<byte[]>();
     foreach(var crt in store.Certificates)
     {
-      // all.Add(crt.ToString());
-      all.Add(crt.Subject);
+      all.Add(crt.GetRawCertData());
     }
     return all;
   }
--- edge/index.js
+++ edge/index.js
@@ -1,6 +1,21 @@
 var path = require('path')
 var edge = require('edge');
 
+forge = require('node-forge')
+pki = forge.pki
+asn1 = forge.asn1
+
 var enumCA = edge.func(path.join(__dirname, 'enum.cs'))
 
-console.log(enumCA(0, true))
+enumCA(0, true).forEach(unDer)
+
+function unDer(der)
+{
+  var cer = pki.certificateFromAsn1(asn1.fromDer(der.toString('binary')))
+  console.log(cer.subject.attributes.map(rdn).join(''))
+}
+
+function rdn(rdn)
+{
+  return "/" + (rdn.shortName || rdn.name || rdn.type) + "=" + rdn.value
+}
