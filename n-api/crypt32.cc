166df21a76a558d129b3b58b50427dc1626cb404
Stas Ukolov <ukoloff@gmail.com>
2019-04-06 12:46:01 +0500

    CLean N-API interfacing

--- n-api/crypt32.cc
+++ n-api/crypt32.cc
@@ -8,19 +8,21 @@ class Crypt32 : public Napi::ObjectWrap<Crypt32> {
  public:
   Crypt32(const Napi::CallbackInfo& info);
 
-  static void Init(Napi::Env env);
-  static Napi::Object New(Napi::Env env);
-
  private:
   HCERTSTORE hStore;
   PCCERT_CONTEXT pCtx = nullptr;
   static Napi::FunctionReference constructor;
+
+  static HCERTSTORE openStore(const Napi::CallbackInfo&);
 
   Napi::Value next(const Napi::CallbackInfo&);
   Napi::Value done(const Napi::CallbackInfo&);
 
   char* begin() const { return (char*)pCtx->pbCertEncoded; }
   char* end() const { return begin() + pCtx->cbCertEncoded; }
+
+  friend Napi::Object crypt32nit(Napi::Env, Napi::Object);
+  friend Napi::Object crypt32exports(const Napi::CallbackInfo&);
 };
 
 // Implementation
@@ -28,8 +30,11 @@ class Crypt32 : public Napi::ObjectWrap<Crypt32> {
 Napi::FunctionReference Crypt32::constructor;
 
 Crypt32::Crypt32(const Napi::CallbackInfo& info)
-    : Napi::ObjectWrap<Crypt32>(info),
-      hStore(CertOpenSystemStoreA(0, "ROOT")) {}
+    : Napi::ObjectWrap<Crypt32>(info), hStore(openStore(info)) {}
+
+HCERTSTORE Crypt32::openStore(const Napi::CallbackInfo&) {
+  return CertOpenSystemStoreA(0, "ROOT");
+}
 
 Napi::Value Crypt32::next(const Napi::CallbackInfo& info) {
   pCtx = CertEnumCertificatesInStore(hStore, pCtx);
@@ -44,32 +49,27 @@ Napi::Value Crypt32::done(const Napi::CallbackInfo& info) {
   return info.Env().Undefined();
 }
 
-void Crypt32::Init(Napi::Env env) {
-  Napi::HandleScope scope(env);
-
-  Napi::Function func = DefineClass(env, "Crypt32",
-                                    {
-                                        InstanceMethod("done", &Crypt32::done),
-                                        InstanceMethod("next", &Crypt32::next),
-                                    });
-
-  constructor = Napi::Persistent(func);
-  constructor.SuppressDestruct();
-}
-
-Napi::Object Crypt32::New(Napi::Env env) {
-  Napi::EscapableHandleScope scope(env);
-  Napi::Object obj = constructor.New({});
+Napi::Object crypt32exports(const Napi::CallbackInfo& info) {
+  Napi::EscapableHandleScope scope(info.Env());
+  std::vector<napi_value> args(info.Length());
+  for (size_t i = 0; i < args.size(); ++i) {
+    args[i] = info[i];
+  }
+  Napi::Object obj = Crypt32::constructor.New(args);
   return scope.Escape(napi_value(obj)).ToObject();
 }
 
-static Napi::Object CreateCrypt32(const Napi::CallbackInfo& info) {
-  return Crypt32::New(info.Env());
+Napi::Object crypt32init(Napi::Env env, Napi::Object) {
+  Napi::HandleScope scope(env);
+
+  Crypt32::constructor = Napi::Persistent(
+      Crypt32::DefineClass(env, "Crypt32",
+                           {
+                               Crypt32::InstanceMethod("done", &Crypt32::done),
+                               Crypt32::InstanceMethod("next", &Crypt32::next),
+                           }));
+  Crypt32::constructor.SuppressDestruct();
+  return Napi::Function::New(env, crypt32exports, "Crypt32");
 }
 
-static Napi::Object Init(Napi::Env env, Napi::Object) {
-  Crypt32::Init(env);
-  return Napi::Function::New(env, CreateCrypt32, "Crypt32");
-}
-
-NODE_API_MODULE(crypt32, Init)
+NODE_API_MODULE(crypt32, crypt32init)
